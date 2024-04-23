1159b9cf939858baf166cefb77edfab6f33c82f6
Francesco Di Gennaro <francesco.dgennaro@gmail.com>
2024-04-23 12:47:28 +0200

    fix connect graphs doubles

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ import random as rd
 def main() :
 
 
-    net = Network()             
+    net = Network()
     net.nodes_creation()
-    net.graph_creation()
+    net.graph_creation_2()
     # for key, val in (net.connections).items() :
     #     print(f" {key}      -->      {val} \n")
     # print(net.main_dfs())
-    print(net.connections)
+    
+    for node in net.connections:
+        print(f"{node.type : <6} {node.id : <3} | {net.connections[node]}")
 
 if __name__ == "__main__" :
     print("\033c")
