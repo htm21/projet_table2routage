d3760cda6aba0fac5e795fbb7c099f7337eb0715
Francesco Di Gennaro <francesco.dgennaro@gmail.com>
2024-04-27 22:51:12 +0200

    mac test

--- Modules/app.py
+++ Modules/app.py
@@ -42,7 +42,7 @@ class App(object):
 
         self.parent.geometry(f"{self.gui_w}x{self.gui_h}+{self.x}+{self.y}")      
         self.parent.title("Project Table Routage")
-        self.parent.resizable(False, False)
+        self.parent.resizable(True, True)
         self.parent.update()
 
         # Frames =======================================================================
